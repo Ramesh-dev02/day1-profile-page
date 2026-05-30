d649597deedb3915cce908e518e8aff1a1350596
Ramesh <door_number_26@outlook.com>
2026-05-31 00:52:32 +0530

    day-3 of my portfolio

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 console.log("website loaded successfully!");
 function showmessage(){
     alert("welcome to my portfolio webpage");
+    alert("you are lucky");
+}
+function popmessage(){
+    alert("hi dude");
 }
